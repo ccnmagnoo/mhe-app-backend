3c42d5e5704ac0abf41bec2b4e4be5e6e78862ad
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-07-23 12:20:25 -0400

    init

---
+++ functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+const db = firebase.firestore;
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
