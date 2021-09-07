5a7f023f4db1d545628291f59595a66a58df03eb
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-09-07 06:42:23 -0300

    fix

--- functions/src/config/mailProvider.ts
+++ functions/src/config/mailProvider.ts
@@ -3,7 +3,7 @@ export const provider = {
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: 'ccamposn@minenergia.cl', // generated ethereal user
+    user: '***@minenergia.cl', // generated ethereal user
     pass: '****', // generated ethereal password
   },
 };
