f10b8f16049951969204c918e722a62cfb863398
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-11-09 23:12:34 -0300

    params query on request

--- functions/src/routes/roomReport.routes.ts
+++ functions/src/routes/roomReport.routes.ts
@@ -6,23 +6,37 @@ import { dbKey as key } from './../Tools/databaseKeys';
 const router = Router();
 module.exports = router;
 
-router.get(`/api/rooms/:key`, async (req, res) => {
+router.get(`/api/rooms`, async (req, res) => {
+  //res:https://stackoverflow.com/questions/17007997/how-to-access-the-get-parameters-after-in-express
+
   /**
    * @api router for powerBI Report
    */
-  if (req.params.key !== 'yyz')
+
+  if (req.query.key !== 'yyz')
     return res.status(500).json({ numberOfRooms: 'wrong key' });
+  if (req.query.key === undefined)
+    return res.status(500).json({ numberOfRooms: 'year undefined' });
 
   try {
+    //defining filter query parameter
+    const yearFilter = req.query.year as string;
+
+    //firebase 🔥🔥🔥
     const refRoom = db
       .collection(`${key.act}/${key.uid}/${key.room}`)
       .withConverter(iClassroomConverter);
 
     const query = await refRoom.get();
 
-    const rooms: IClassroom[] = query.docs.map((snapshot) => {
-      return snapshot.data();
-    });
+    //building array from query Snapshot
+    const rooms: IClassroom[] = query.docs
+      .map((snapshot) => {
+        return snapshot.data();
+      })
+      .filter((room) => {
+        return room.placeActivity.date.getFullYear().toString() === yearFilter;
+      });
 
     return res.status(200).json({ numberOfRooms: rooms.length });
   } catch (error) {
