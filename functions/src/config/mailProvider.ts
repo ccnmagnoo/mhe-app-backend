25c4d5e5b9f740a01df3ac29c5bc8eef31603146
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-01-13 10:01:01 -0300

    .evn variables

--- functions/src/config/mailProvider.ts
+++ functions/src/config/mailProvider.ts
@@ -1,9 +1,13 @@
+require('dotenv').config({
+  path: '.env.production',
+});
+
 export const provider = {
   host: 'smtp.gmail.com',
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
-    user: 'conbuenaenergia@dvt.cl', // user
-    pass: 'CC.****2021', // password
+    user: process.env.EMAIL_USER, // user
+    pass: process.env.EMAIL_PASS, // password
   },
 };
