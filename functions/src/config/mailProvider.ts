10bfd76271820ae71df7ebcf3749e747f3716aad
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-04-28 10:32:05 -0400

    import enviroment values inside index

--- functions/src/config/mailProvider.ts
+++ functions/src/config/mailProvider.ts
@@ -1,13 +1,25 @@
-require('dotenv').config({
-  path: '.env.production',
-});
+// require('dotenv').config({
+//   path: '.env.production',
+// });
 
-export const provider = {
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for 465, false for other ports
-  auth: {
-    user: process.env.EMAIL_USER, // user
-    pass: process.env.EMAIL_PASS, // password
-  },
-};
+interface Conn {
+  host: string;
+  port: number;
+  secure: boolean;
+  auth: { user?: string; pass?: string };
+}
+
+export function provider(user?: string, pass?: string) {
+  const conn: Conn = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: user,
+      pass: pass,
+      // user: process.env.EMAIL_USER, // user
+      // pass: process.env.EMAIL_PASS, // password
+    },
+  };
+  return conn;
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -111,16 +111,21 @@ exports.onCreateSuscription = functions.firestore
     return true;
   });
 
+require('dotenv').config({
+  path: '.env.production',
+});
+
 /**
  * @function mailer nodemailer services to send basic
  * information of activities to suscribed users.
  */
 export async function mailer(room: IClassroom | undefined, benf: IBeneficiary) {
-  let transporter = nodemailer.createTransport(provider);
+  const prov = provider(process.env.EMAIL_USER, process.env.EMAIL_PASS);
+  let transporter = nodemailer.createTransport(prov);
 
   try {
     let info = await transporter.sendMail({
-      from: `"Equipo Con Buena Energía 💚" <${provider.auth.user}>`, // sender address
+      from: `"Equipo Con Buena Energía 💚" <${prov.auth.user}>`, // sender address
       to: benf.email, // list of receivers
       subject: 'Inscripción Con Buena Energía', // Subject line
       html: emailModel(room, benf), // html body
